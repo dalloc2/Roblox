75e350a7c09b1745928a31802e5f5ec05dbfb629
dalloc <128258555+dalloc2@users.noreply.github.com>
2024-06-21 18:45:54 +0100

    Update Info.lua

--- Miscellaneous/Info.lua
+++ Miscellaneous/Info.lua
@@ -1,1 +1,1 @@
-return { Version = 'v0.1b',  Message = 'refresh buggy doe fix later!', Eligible = false }
+return { Version = 'v0.1b',  Message = 'refresh buggy doe fix later!', Eligible = false, Blacklisted = {1} }
