878a80ae987afd38b5af0da26ce1660e041de372
dalloc <128258555+dalloc2@users.noreply.github.com>
2024-06-30 01:46:30 +0100

    Update Info.lua

--- Miscellaneous/Info.lua
+++ Miscellaneous/Info.lua
@@ -1,2 +1,2 @@
 --You shall not know what this is for!!!
-return { Version = 'v0.2b',  Message = 'HI DOORS WORK NOW !!!', Eligible = false, Blacklisted = {1} }
+return { Version = 'v0.2b',  Message = 'Admins who toggle and alt rejoins will now have the toggle back on.', Eligible = false, Blacklisted = {1} }
